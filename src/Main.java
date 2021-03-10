79092992bb745cd24e5d3cc3cb6a600f9e38df07
Kirill Popov <qer141@bk.ru>
2021-03-10 14:47:40 +0400

    add files

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int transfer = 999;
+        long bonus = transfer / 100;
+        if (transfer < 1000) {
+            System.out.println("the minimum payment for crediting bonuses is 1000 rubles");
+        } else {
+            System.out.println(bonus);
+
+        }
+    }
+}
